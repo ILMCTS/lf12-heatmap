10e1117971c8c25182dd4c8ab41f96759b3cc22f
Leander Scherwinsky <leander.scherwinsky@mention.de>
2024-11-20 14:26:49 +0100

    push class test data (;

---
+++ src/lib/class-tests-data.ts
@@ -0,0 +1,37 @@
+export default [
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-10-10"
+    },
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-10-11"
+    },
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-11-20"
+    },
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-11-15"
+    },
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-11-14"
+    },
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-11-19"
+    },
+    {
+        "subj": "LF08",
+        "class": "FOS 41",
+        "date": "2024-10-14"
+    }
+];
